5c272b8c6d2fbaa7a7316c53ca5da3636df94139
Dexter De Leon <dexdeleon@DextAir.local>
2018-11-12 16:09:13 +0000

    Cleaned up game logic code by adding functions; checkForMatch and flipCard.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,25 @@
 console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[2];
-var cardTwo = cards[3];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2){
+var checkForMatch = function(){
+	//check to see if flipped cards match
 	if (cardsInPlay[0]===cardsInPlay[1]){
-		alert("You found a match!");
-	}
-	else {
-		alert("Sorry, try again.");
+			alert("You found a match!");
+		}
+		else {
+			alert("Sorry, try again.");
+		}
+}
+var flipCard = function(cardId){
+	//print flipped card to console
+	console.log("User flipped " + cards[cardId]);
+	//add flipped card to cardsInPlay array
+	cardsInPlay.push(cards[cardId]);
+	//check if there have been 2 cards flipped
+	if (cardsInPlay.length === 2){
+		checkForMatch();
 	}
 }
+
+flipCard(0);
+flipCard(2);
