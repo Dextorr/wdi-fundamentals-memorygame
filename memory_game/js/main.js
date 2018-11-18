f49b195c9e92edf75f89e8b42616e4fde5a428c4
Dexter De Leon <dexdeleon@DextAir.local>
2018-11-18 16:49:58 +0000

    Added reset button and score to index.html to display player's score and give option to restart the game prior to completion. Added styles to style.css for new html elements and changed display of the game board to flex with appropriate rules so that newly added cards wrap neatly beneath the existing row of four. Updated main.js to include code for resetting the board on click of the reset button. Also code for tracking the player's score and displaying it. Included extra card objects in the cards array. Updated functions flipCard and checkForMatch to remove event listeners to prevent clicks until the next turn of the game and an alert when the game is over displaying the final score.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,27 +1,59 @@
 console.log("Up and running!");
+//array of card objects
 var cards = [
 	{
 		rank: "queen",
 		suit: "hearts",
-		cardImage: "images/queen-of-hearts.png"
+		cardImage: "images/queen_of_hearts.png"
 	},
 	{
 		rank: "queen",
 		suit: "diamonds",
-		cardImage: "images/queen-of-diamonds.png"
+		cardImage: "images/queen_of_diamonds.png"
 	},
 	{
 		rank: "king",
 		suit: "hearts",
-		cardImage: "images/king-of-hearts.png"
+		cardImage: "images/king_of_hearts.png"
 	},
 	{
 		rank: "king",
 		suit: "diamonds",
-		cardImage: "images/king-of-diamonds.png"
+		cardImage: "images/king_of_diamonds.png"
+	},
+	{
+		rank: "jack",
+		suit: "diamonds",
+		cardImage: "images/jack_of_diamonds.png"
+	},
+	{
+		rank: "jack",
+		suit: "hearts",
+		cardImage: "images/jack_of_hearts.png"
+	},
+	{
+		rank: "10",
+		suit: "diamonds",
+		cardImage: "images/10_of_diamonds.png"
+	},
+	{
+		rank: "10",
+		suit: "hearts",
+		cardImage: "images/10_of_hearts.png"
 	}
 ];
+//empty array to store flipped cards
 var cardsInPlay = [];
+
+//variable to hold player's score
+var score = 0;
+
+//NodeList of cards
+var cardElements = document.getElementsByTagName('img');
+
+var updateScore = function(){
+	document.getElementById('score').textContent = score;
+}
 
 var shuffle = function(deck){
 	//Keep count of cards left to shuffle
@@ -46,13 +78,67 @@ var shuffle = function(deck){
 //Shuffle deck before it's displayed on the game board
 cards = shuffle(cards);
 
+var tryAgain = function(){
+	//change message text to indicate start of next turn
+	document.getElementById('gameHeader').innerHTML = "Click another two cards to flip."
+	//get selected cards
+	var selected = document.getElementsByClassName('selected');
+	//for each selected card...
+	for (var i=0;i<2;i++){
+		//reset card image to card back and remove class 'selected'
+		selected[0].setAttribute('src', 'images/back.png');
+		selected[0].classList.remove('selected');
+	}
+	//reapply event listeners for next turn
+	for (var i=0;i<cardElements.length;i++){
+		cardElements[i].addEventListener('click', flipCard)
+	}
+	//empty cardsInPlay array
+	cardsInPlay = [];
+}
+
 var checkForMatch = function(){
+	//remove event listeners from cards to prevent further clicks before match check
+	for (var i=0;i<cardElements.length;i++){
+		cardElements[i].removeEventListener('click', flipCard);
+	}	
 	//check to see if flipped cards match
 	if (cardsInPlay[0].rank===cardsInPlay[1].rank){
+			//change game header message
 			document.getElementById('gameHeader').textContent = "You found a match!";
+			//update the score
+			score += 50;
+			updateScore();
+			//empty cardsInPlay array
+			cardsInPlay = [];
+			//for each card image...
+			for (var i=0;i<cardElements.length;i++){
+				//if the card has the 'selected' class...
+				if (cardElements[i].classList.contains('selected')){
+					//...remove it...
+					cardElements[i].classList.remove('selected');
+					//...and mark it as scored
+					cardElements[i].classList.add('scored');
+				}
+				//if the card has not already been scored...
+				else if (!cardElements[i].classList.contains('scored')){
+					cardElements[i].addEventListener('click', flipCard);
+				}		
+			}
+			//check if the game is over
+			if (document.getElementsByClassName('scored').length === cards.length){
+				//and display the game end message as an alert 
+				alert("Congratulations! You scored " + score + " points!\n" + "Click 'OK' to play again.");
+				resetBoard();
+			}
 		}
 		else {
-			document.getElementById('gameHeader').textContent = "Sorry, try again.";
+			//update the score
+			score -= 10;
+			updateScore();
+			//show button to try again
+			document.getElementById('gameHeader').innerHTML = "Sorry, click <a id='tryAgainButton'>here</a> to try again.";
+			document.getElementById('tryAgainButton').addEventListener('click', tryAgain);
 		}
 }
 
@@ -70,18 +156,40 @@ var flipCard = function(){
 	cardsInPlay.push(cards[cardId]);
 	//check if there have been 2 cards flipped
 	if (cardsInPlay.length === 2){
+		//if 2 cards have been flipped, check for a match
 		checkForMatch();
 	}
 }
 
 var createBoard = function(){
 	for (var i=0;i<cards.length;i++){
+		//create card img element
 		var cardElement = document.createElement('img');
+		//set initial image to the back.png file
 		cardElement.setAttribute('src', 'images/back.png');
+		//set the card's data-id to its index in the cards array
 		cardElement.setAttribute('data-id', i);
+		//add the event listener to flip the card to the card element
 		cardElement.addEventListener('click', flipCard);
+		//add the card element to the game board
 		document.getElementById('game-board').appendChild(cardElement);
 	}
 }
 
+var resetBoard = function(){
+	//reshuffle the deck
+	cards = shuffle(cards);
+	//clear the board
+	for (var i=0;i<cards.length;i++){
+		document.getElementById('game-board').removeChild(cardElements[0]);
+	}
+	//reset the score
+	score = 0;
+	updateScore();
+	//recreate the board
+	createBoard();
+}
+
 createBoard();
+//add the reset event listener to the reset button
+document.getElementById('reset-button').addEventListener('click', resetBoard);
